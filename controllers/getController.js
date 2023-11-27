5cdba4c87988255ee0faf9a52f5da31235834a2c
Fırat Yolal <firatyll213@gmail.com>
2023-11-28 01:08:17 +0300

    corrections

--- controllers/getController.js
+++ controllers/getController.js
@@ -15,16 +15,27 @@ exports.getLogs = async (req, res) => {
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
+        let { from, to, limit } = req.query;
         let log = user.log;
+        if (from) {
+            log = log.filter((exercise) => new Date(exercise.date) >= new Date(from));
+        }
+        if (to) {
+            log = log.filter((exercise) => new Date(exercise.date) <= new Date(to));
+        }
+        if (limit) {
+            log = log.slice(0, parseInt(limit, 10));
+        }
+
         return res.status(200).json({
             _id: user._id,
             username: user.username,
             count: user.count,
-            log : log.map((exercise) => ({
+            log: log.map((exercise) => ({
                 description: exercise.description,
                 duration: exercise.duration,
-                date: new Date(exercise.date).toDateString()
-            }))
+                date: new Date(exercise.date).toDateString(),
+            })),
         });
     } catch (err) {
         res.status(500).json({ error: 'Error getting logs' });
